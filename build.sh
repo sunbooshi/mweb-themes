46b89ad17624fb3cb525941670843659a9b035a5
booshi <sunbooshi@gmail.com>
2025-02-04 21:10:45 +0800

    add release tools.

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 rm -fr dist
-
+npm run download
 npm run compile -- --platform obsidian --distDir dist/obsidian
+
+cp themes.json dist/
+cp highlights.json dist/
 
 mkdir dist/themes
 cp -r dist/obsidian/dark/* dist/themes/
 cp -r dist/obsidian/light/* dist/themes/
 
+cp src/themes/maple.css dist/themes/
+
 cd dist/themes/
 rename 's/obsidian-/mweb-/' *
 
-cd ../..
-tar -czf themes.tar.gz dist/themes
+cd ..
+zip assets.zip themes.json highlights.json themes/* highlights/*
+
+cd ..
